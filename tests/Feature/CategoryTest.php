d2f2da2ab250073f4376a7fbf95b951b47294077
haris2303 <hrsccf102@gmail.com>
2023-11-12 16:53:14 +0900

    first

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -50,4 +50,89 @@ class CategoryTest extends TestCase
         $this->assertEquals("Food", $category->name);
         $this->assertEquals("Food Category", $category->description);
     }
+
+    public function testUpdate()
+    {
+        $this->seed(CategorySeeder::class);
+
+        $category = Category::find("FOOD");
+        $category->name = "Food Updated";
+
+        $result = $category->update();
+        $this->assertTrue($result);
+    }
+
+    public function testSelect()
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $category = new Category();
+            $category->id = "ID $i";
+            $category->name = "Category $i";
+            $category->save();
+        }
+
+        $categories = Category::whereNull("description")->get();
+        $this->assertEquals(5, $categories->count());
+        $categories->each(function ($category) {
+            $this->assertNull($category->description);
+
+            $category->description = "Updated";
+            $category->update();
+        });
+    }
+
+    public function testUpdateMany()
+    {
+        $categories = [];
+        for ($i = 0; $i < 10; $i++) {
+            $categories[] = [
+                "id" => "ID $i",
+                "name" => "Category $i"
+            ];
+        }
+
+        $result = Category::insert($categories);
+        $this->assertTrue($result);
+
+        Category::whereNull("description")->update([
+            "description" => "Updated"
+        ]);
+
+        $total = Category::where("description", "=", "Updated")->count();
+        $this->assertEquals(10, $total);
+    }
+
+    public function testDelete()
+    {
+        $this->seed(CategorySeeder::class);
+
+        $category = Category::find("FOOD");
+        $result = $category->delete();
+        $this->assertTrue($result);
+
+        $total = Category::count();
+        $this->assertEquals(0, $total);
+    }
+
+    public function testDeleteMany()
+    {
+        $categories = [];
+        for ($i = 0; $i < 10; $i++) {
+            $categories[] = [
+                "id" => "ID $i",
+                "name" => "Category $i"
+            ];
+        }
+
+        $result = Category::insert($categories);
+        $this->assertTrue($result);
+
+        $total = Category::count();
+        $this->assertEquals(10, $total);
+
+        Category::whereNull('description')->delete();
+
+        $total = Category::count();
+        $this->assertEquals(0, $total);
+    }
 }
